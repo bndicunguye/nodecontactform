85a1043aed32703c630b2c82a39b60820eb2b9af
Mayenga <bndicunguye@gmail.com>
2020-05-20 17:49:36 -0400

    initial views and public

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+
+const app = express();
+
+//View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//static folder//
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('contact', {
+         layout:false,
+         name: req.body.name,
+         quote: req.body.quote
+    });
+});
+
+app.post('/send', (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Company: ${req.body.company}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>`;
+
+     // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'mail.sendinblue.com',
+    port: 587,
+    // secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'shawn66@ethereal.email', // generated ethereal user
+      pass: 'EFGq49XdFPtUW34TNe', // generated ethereal password
+    },
+    tls:{
+        rejectUnauthorized:false
+    }
+  });
+
+  // send mail with defined transport object
+  let mailOptions = {
+    from: '"Nodemailer contact" <shawn66@ethereal.email>', // sender address
+    to: "bndicunguye@gmail.com", // list of receivers
+    subject: "Node contact request ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: output // html body
+  };
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) =>{
+    if (error) {
+        return console.log(error);
+    }
+})
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  
+  res.render('contact', {msg:'Email has been sent'});
+});
+
+
+
+app.listen(3000, () => console.log('server started ma nigga...'));
+
